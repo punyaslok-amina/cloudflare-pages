f5059402073aaefa9f926fb7ca670ddd199c7f76
Punyasloka Parida <punyasloka.parida@seba.swiss>
2025-03-19 17:44:29 +0530

    Pages

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			}
+		})
+	}
+};
+
+export default config;
