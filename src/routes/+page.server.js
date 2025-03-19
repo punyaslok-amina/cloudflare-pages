eedd842f8ea6a8a1f314e5cd28b94e4a44341095
Punyasloka Parida <punyasloka.parida@seba.swiss>
2025-03-19 19:00:30 +0530

    Backend pages

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,13 +1,8 @@
 import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 var greetings = {
-	'lang': "bonjour-de",
-};
-if (!building) {
-	greetings = {
 	'lang': env.LANGUAGE,
 };
-}
 export function load({ params }) {
 	return {
 		greeting: greetings[params.lang ?? 'lang']
